5bbd15f8ec92e914cd440d53fd1263240400707b
alfonsomiralles <alfonsomiralles1@gmail.com>
2023-04-10 21:04:39 +0200

    startproject

---
+++ DjangoChatBot/DjangoChatBot/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for DjangoChatBot project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChatBot.settings')
+
+application = get_asgi_application()
